8b4eeb21e43fd0f2ccce80eeea4856453af1b327
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-16 13:46:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskInput = document.querySelector("input#new-task-description");
+  let tasks = [];
+  document
+    .querySelector("input#submit-task")
+    .addEventListener("click", function (e) {
+      let task = taskInput.value;
+      tasks.push(task);
+      displayTasks(tasks);
+      taskInput.value = "";
+      e.preventDefault();
+    });
 });
+
+function displayTasks(tasks) {
+  const taskList = document.querySelector("ul#tasks");
+  let taskItems = tasks.map((task) => `<li>${task}</li>`);
+  taskList.innerHTML = taskItems.join("\n");
+}
